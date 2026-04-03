656105a0cd1017dae3c9115f86f4e54e5949ec61
rflnavas <rafanavas2005@gmail.com>
2026-04-03 22:47:24 +0200

    Name tests in green

--- src/main/java/com/rnr/domain/client/Name.java
+++ src/main/java/com/rnr/domain/client/Name.java
@@ -1,9 +1,25 @@
 package com.rnr.domain.client;
 
+import com.rnr.error.EmptyValueException;
+import com.rnr.error.InvalidValue;
+import com.rnr.utils.StringUtility;
+
+import java.util.Objects;
+
 public class Name {
 
-    private String name;
+    public static final int MAX_NAME_LENGTH = 80;
+    private final String name;
+
     private Name(String name) {
+        if(StringUtility.isEmpty(name)) {
+            throw new EmptyValueException("Name cannot be blank");
+        }
+        if(name.length() > MAX_NAME_LENGTH) {
+            throw new InvalidValue(
+                    String.format("%s has more than %d characters -> %d",
+                            name, MAX_NAME_LENGTH, name.length()));
+        }
         this.name = name;
     }
 
@@ -15,4 +31,14 @@ public class Name {
         return name;
     }
 
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Name name1)) return false;
+        return Objects.equals(name, name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
 }
--- src/test/java/domain/client/NameTest.java
+++ src/test/java/domain/client/NameTest.java
@@ -1,9 +1,12 @@
 package domain.client;
 
 import com.rnr.domain.client.Name;
+import com.rnr.error.EmptyValueException;
+import com.rnr.error.InvalidValue;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
@@ -12,8 +15,9 @@ public class NameTest {
 
     @ParameterizedTest
     @MethodSource("invalidNames")
-    void shouldNamesNotBeCreatedWhenInvalidValues(String invalidName){
-        Assertions.assertThatThrownBy(() -> Name.as(invalidName)).hasMessage(invalidName + " is not a valid name");
+    void shouldNamesNotBeCreatedWhenInvalidValues(String invalidName, Class<? extends RuntimeException> exception){
+        Assertions.assertThatThrownBy(() -> Name.as(invalidName))
+                .isInstanceOf(exception);
     }
 
     @ParameterizedTest
@@ -30,9 +34,10 @@ public class NameTest {
        Assertions.assertThat(john).isEqualTo(Name.as("John"));
     }
 
-    static Stream<String> invalidNames() {
-        return Stream.of("", "   ",
-                "a very very very very looooooooooooooooooooooooooooooong name");
+    static Stream<Arguments> invalidNames() {
+        return Stream.of(Arguments.of("", EmptyValueException.class),
+                Arguments.of("   ", EmptyValueException.class),
+                Arguments.of("a veeeeery veeeery veeery veery extreeeemely looooooooooooooooooooooooooooooong name", InvalidValue.class));
     }
 
     static Stream<String> validNames() {
